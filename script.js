ffd95bc28167daad5feafb229926216c26dd482f
Emmanuel Viana Acácio Sales <202511900011@ifto.local>
2026-09-10 09:44:51 -0300

    Atualiza projeto e inclui novos arquivos da atividade

---
+++ script.js
@@ -0,0 +1,13 @@
+// Seleciona o botão do menu pelo ID
+const botaoMenu = document.querySelector("#btn-menu");
+
+// Seleciona o menu pela classe
+const menu = document.querySelector(".nav");
+
+// Quando o botão for clicado
+botaoMenu.addEventListener("click", function () {
+
+    // Adiciona ou remove a classe "ativo"
+    menu.classList.toggle("ativo");
+
+});
